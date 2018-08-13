05e51fef87d20d4532c077c2cb7b89838a20bee1
Pramode <Pramode@pandit-pramode>
2018-08-13 16:10:05 +0530

    ok

---
+++ src/test/java/com/javacodegeeks/examples/junitmavenexample/DatabaseTest.java
@@ -0,0 +1,11 @@
+package com.javacodegeeks.examples.junitmavenexample;
+
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void checkDatabase() {
+	System.out.println("Database is  OK ...");
+	}
+}
---
+++ src/test/java/com/javacodegeeks/examples/junitmavenexample/HealthTest.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks.examples.junitmavenexample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HealthTest {
+	
+	@Test
+	public void checkHealth() {
+	System.out.println("Health OK ...");
+}
+
+}
